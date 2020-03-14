23ad7260b485b31631e1197c1fd814c6b90a4788
Richard Kell <rakqhn@mst.edu>
2020-03-14 05:09:48 -0500

    adjusted failed run output message

--- runOnInputFolder.sh
+++ runOnInputFolder.sh
@@ -4,8 +4,8 @@ numberArguments=${#@}
 
 if [[ $numberArguments < 3 ]]
 then
-echo -e "Usage:\n./runTests.sh <inputFileFolder> <outputFileFolder> [command] <executable> [flags] [arguments]"
-echo "example: ./runTests.sh inputFolder outputFolder python3 pythonTest.py 1 2 3"
+echo -e "Usage:\n./runOnInputFolder.sh <inputFileFolder> <outputFileFolder> [command] <executable> [flags] [arguments]"
+echo "example: ./runOnInputFolder.sh inputFolder outputFolder python3 pythonTest.py 1 2 3"
 else
 	inputFolder=$1
 	outputFolder=$2
